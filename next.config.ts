e8a1f060da3db47d9c751b4d73692c9ddcbb7494
Joseph Adedeji Adewunmi <adedejiosvaldo@gmail.com>
2024-12-13 12:32:37 +0100

    Feature ->Changed some nextjs webpack setting

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,16 @@ const nextConfig: NextConfig = {
   output: "standalone",
   distDir: ".next",
   experimental: {},
+  webpack: (config) => {
+    // Handle OSX module resolution
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve?.fallback,
+        "osx-temperature-sensor": false,
+      },
+    };
+    return config;
+  },
 };
 export default nextConfig;
